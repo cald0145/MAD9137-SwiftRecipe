3e4040c636d0f60cb18aa598e9791b93de4ad259
Jay Calderon <cald0145@algonquinlive.com>
2024-10-23 17:22:13 -0400

    Initialize new Xcode project with Swift and SwiftUI - Initial file template for a new project within Xcode

---
+++ SwiftRecipe/SwiftRecipeTests/SwiftRecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRecipeTests.swift
+//  SwiftRecipeTests
+//
+//  Created by Jay Calderon on 2024-10-23.
+//
+
+import Testing
+@testable import SwiftRecipe
+
+struct SwiftRecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
